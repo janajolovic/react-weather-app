4d43c9858ceb8e92a4f793856b3aa52a9762288f
Jana Jolovic <janajolovic04@gmail.com>
2022-01-21 23:59:26 +0100

    search city

--- src/App.js
+++ src/App.js
@@ -5,16 +5,26 @@ import axios from "axios";
 function App() {
   const API_key = "b8bf7f69e6001c16ade11d8120a02f4d";
   const [data, setData] = useState({});
+  const [city, setCity] = useState("Novi Pazar");
 
   const getData = async () => {
     try {
       let response = await axios
-        .get(`https://api.openweathermap.org/data/2.5/weather?q=Novi%20Pazar&units=metric&appid=${API_key}`)
+        .get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_key}`)
         setData(response.data)
-        console.log(response.data)
       } catch (err) {
         console.log(err)
       }
+    }
+
+    const inputHandler = (e) => {
+      e.preventDefault();
+      setCity(e.target.value);
+    }
+
+    const getCity = () => {
+      getData();
+      setCity("");
     }
     
     useEffect(() => {
@@ -25,8 +35,8 @@ function App() {
       <div className="main_container">
       <div className="header">
         <h1 className="header-text">Weather App</h1>
-        <input type="text" placeholder="Enter city"/>
-        <button>Get weather</button>
+        <input type="text" placeholder="Enter city" value={city} name={city} onChange={(e) => {inputHandler(e)}}/>
+        <button onClick={() => {getCity()}}>Get weather</button>
       </div>
       <div className="weather-card">
         <h3>{data.name}, {data.sys.country}</h3>
